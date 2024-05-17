c9268a3a00b03e832356a2d83a5dcc4051f60156
toamerse <131949379+toamerse@users.noreply.github.com>
2024-05-17 13:27:07 +0800

    Add files via upload

---
+++ ITSA_8.cpp
@@ -0,0 +1,83 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int a;
+    cin >> a;
+    switch (a)
+    {
+    case 0:
+        cout << "*****" << endl;
+        cout << "*   *" << endl;
+        cout << "*   *" << endl;
+        cout << "*   *" << endl;
+        cout << "*****" << endl;
+        break;
+    case 1:
+        cout << "    *" << endl;
+        cout << "    *" << endl;
+        cout << "    *" << endl;
+        cout << "    *" << endl;
+        cout << "    *" << endl;
+        break;
+    case 2:
+        cout << "*****" << endl;
+        cout << "    *" << endl;
+        cout << "*****" << endl;
+        cout << "*    " << endl;
+        cout << "*****" << endl;
+        break;
+    case 3:
+        cout << "*****" << endl;
+        cout << "    *" << endl;
+        cout << "*****" << endl;
+        cout << "    *" << endl;
+        cout << "*****" << endl;
+        break;
+    case 4:
+        cout << "*   *" << endl;
+        cout << "*   *" << endl;
+        cout << "*****" << endl;
+        cout << "    *" << endl;
+        cout << "    *" << endl;
+        break;
+    case 5:
+        cout << "*****" << endl;
+        cout << "*    " << endl;
+        cout << "*****" << endl;
+        cout << "    *" << endl;
+        cout << "*****" << endl;
+        break;
+    case 6:
+        cout << "*    " << endl;
+        cout << "*    " << endl;
+        cout << "*****" << endl;
+        cout << "*   *" << endl;
+        cout << "*****" << endl;
+        break;
+    case 7:
+        cout << "*****" << endl;
+        cout << "    *" << endl;
+        cout << "    *" << endl;
+        cout << "    *" << endl;
+        cout << "    *" << endl;
+        break;
+    case 8:
+        cout << "*****" << endl;
+        cout << "*   *" << endl;
+        cout << "*****" << endl;
+        cout << "*   *" << endl;
+        cout << "*****" << endl;
+        break;
+    case 9:
+        cout << "*****" << endl;
+        cout << "*   *" << endl;
+        cout << "*****" << endl;
+        cout << "    *" << endl;
+        cout << "    *" << endl;
+        break;
+    }
+    return 0;
+}
