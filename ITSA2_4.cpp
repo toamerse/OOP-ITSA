8e543ae2f7a94289b3cd0184c427a77b7b52c237
toamerse <131949379+toamerse@users.noreply.github.com>
2024-05-17 13:55:55 +0800

    Add files via upload

---
+++ ITSA2_4.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <iomanip>
+#include <cmath>
+
+using namespace std;
+
+int main()
+{
+    int times;
+    cin >> times;
+    int a[times][2];
+    for (int i = 0; i < times; i++)
+    {
+        cin >> a[i][0] >> a[i][1];
+    }
+    for (int i = 0; i < times; i++)
+    {
+        cout << a[i][0] + a[i][1] << endl;
+    }
+
+    return 0;
+}
